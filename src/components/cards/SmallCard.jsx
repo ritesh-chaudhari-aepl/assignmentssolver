779aaf7c980a499fe97bcbdc18f74cf0b10a2d9f
ritesh-chaudhari-aepl <techteam@urdoer.com>
2023-06-28 16:09:01 +0530

    card changes

--- src/components/cards/SmallCard.jsx
+++ src/components/cards/SmallCard.jsx
@@ -6,12 +6,15 @@ const SmallCard = ({ icon, bgColor, iconColor, title }) => {
     <div
       className={`card shadow-sm bg-white rounded-md w-[160px] h-[130px] flex flex-col items-center justify-center`}
     >
-      <div className={`h-16 w-16 mb-3 rounded-full ${bgColor} flex items-center justify-center`}>
+      <div
+        className={`h-16 w-16 mb-3 rounded-full ${bgColor} flex items-center justify-center`}
+      >
         {React.cloneElement(icon, { color: iconColor })}
       </div>
       <p className="font-bold text-sm sm:text-sm">{title}</p>
     </div>
   );
 };
+
 
 export default SmallCard;
